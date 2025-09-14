d731e14ebbc1d65c421951fd36f4f65297bd41a2
DEYVID4562 <T01327H@MS.UPLA.EDU.PE>
2025-09-14 06:49:26 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+package com.empresa;
+
+public class Main {
+    public static void main(String[] args) {
+        InversionFeria inversion = new InversionFeria(10000.0); // Ejemplo: 10,000 soles
+        inversion.mostrarResumen();
+    }
+}
